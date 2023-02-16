62bc4404349150350e51136a56cc71b3c5026833
ziyacan <ziyacanistanbullu@gmail.com>
2023-02-16 23:58:00 +0300

    main route changed.

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Crypto from "./pages/crypto";
-import Trending from "./pages/trending";
-import Saved from "./pages/saved";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import { extendTheme } from "@chakra-ui/react";
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Home />,
-//     children: [
-//       {
-//         path: "/",
-//         element: <Crypto />,
-//       },
-//       {
-//         path: "/trending",
-//         element: <Trending />,
-//       },
-//       {
-//         path: "/saved",
-//         element: <Saved />,
-//       },
-//     ],
-//   },
-// ]);
-
 
 const customTheme = extendTheme({
   components: {
@@ -53,8 +27,7 @@ const customTheme = extendTheme({
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <RouterProvider router={router} /> */}
-    <ChakraProvider  theme={customTheme}>
+    <ChakraProvider theme={customTheme}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
