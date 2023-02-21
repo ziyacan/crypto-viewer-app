2f4456a6a0b20f29c4419ae4dc305766caaded1e
Ziya <37026328+ziyacan@users.noreply.github.com>
2023-02-21 14:01:15 +0300

    C:/Program Files/Git/ removed from applayout router

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import AppLayout from "@/layouts/AppLayout";
 import CryptoDetails from "@/components/CryptoDetails";
-import Trending from "../pages/trending";
-import Saved from "../pages/saved";
-import Crypto from "../pages/crypto";
+import Trending from "@/pages/trending";
+import Saved from "@/pages/saved";
+import Crypto from "@/pages/crypto";
 
 const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<AppLayout />}>
+      <Route element={<AppLayout />}>
         <Route index element={<Crypto />} />
         <Route path="/:coinId" element={<CryptoDetails />} />
         <Route path="/trending" element={<Trending />} />
