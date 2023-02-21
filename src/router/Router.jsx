208f3de19e1be9c6f8d466519c9de9d2cba95b22
Ziya <37026328+ziyacan@users.noreply.github.com>
2023-02-21 14:23:29 +0300

    trending coins changed

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import AppLayout from "@/layouts/AppLayout";
 import CryptoDetails from "@/components/CryptoDetails";
-import Trending from "@/pages/trending";
 import Saved from "@/pages/saved";
 import Crypto from "@/pages/crypto";
+import TrendingCoins from './../components/TrendingCoins';
 
 const Router = () => {
   return (
@@ -12,7 +12,7 @@ const Router = () => {
       <Route element={<AppLayout />}>
         <Route path="/" element={<Crypto />} />
         <Route path="/:coinId" element={<CryptoDetails />} />
-        <Route path="/trending" element={<Trending />} />
+        <Route path="/trending" element={<TrendingCoins />} />
         <Route path="/saved" element={<Saved />} />
       </Route>
     </Routes>
